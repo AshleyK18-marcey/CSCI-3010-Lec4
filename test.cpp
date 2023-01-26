b09b6050138ce056bd200fe46e5dd8c0b8c058e6
AshleyK18-marcey <akastler18@gmail.com>
2023-01-26 20:08:32 +0000

    added test case

--- test.cpp
+++ test.cpp
@@ -13,8 +13,11 @@ TEST_CASE ( "Sums are computed", "[Sum]") {
 }
 TEST_CASE ( "Products are computed", "[Product]"){
     std::vector<double> testN = {8.5, 10, 3, 5};
+    std::vector<double> testN2 = {3.4, 11, 2.2, 5.6};
     double result = Product(testN);
+    double result2 = Product(testN2);
     REQUIRE ( result == 1275);
+    REQUIRE ( result2 == 460.768);
 }
 TEST_CASE ( "Add n to all elements in the vector", "[AddN]"){
     std::vector<int> testAdd = {5, 8, 2, 6};
